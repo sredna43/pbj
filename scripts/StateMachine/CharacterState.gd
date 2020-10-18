32a57b00ca72278b00dcb155df4820b87e464f61
Anders <sredna43@gmail.com>
2020-10-18 10:21:37 -0500

    Adding State Machine

---
+++ scripts/StateMachine/CharacterState.gd
@@ -0,0 +1,11 @@
+# CharacterState.gd class definition for character states
+
+extends Node2D
+
+class_name CharacterState
+
+func handle_input(_event):
+    pass
+
+func update_state(_delta):
+    pass
